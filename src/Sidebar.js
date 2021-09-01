50485ae9870053bb6e7325264bc3cff51fe06052
Siddharth Garewal <sidgarewal@gmail.com>
2021-09-01 12:19:57 +0530

    Initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Sidebar.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar_header">
+                <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlrZqTCInyg6RfYC7Ape20o-EWP1EN_A8fOA&usqp=CAU" />
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA8Y-GxHzBsZ9L3j6IgFExuMUJFcaSxvu8",
+    authDomain: "whatsapp-mern-d18bf.firebaseapp.com",
+    projectId: "whatsapp-mern-d18bf",
+    storageBucket: "whatsapp-mern-d18bf.appspot.com",
+    messagingSenderId: "669033985887",
+    appId: "1:669033985887:web:b262de897f0d86c88ae8ea"
+  };
